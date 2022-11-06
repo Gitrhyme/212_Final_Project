97049ca4ddda210933b9669937b4d69829fe5891
samgitmaster <90435153+samgitmaster@users.noreply.github.com>
2022-11-06 12:15:44 -0500

    Update sort.cpp

--- Sorting Final/sort.cpp
+++ Sorting Final/sort.cpp
@@ -40,6 +40,60 @@ void Sort::selection_sort(){
     }
 }
 
+void Sort::merge_sort(std::vector<int>& list){
+    int list_size = list.size();
+    //There is only one element
+    if(list_size < 2){
+        return;
+    }
+    //Split elements_list in half as left and right
+    int middle = list_size/2;
+    std::vector<int> left;
+    std::vector<int> right;
+    //Fill left and right
+    for(int i = 0; i < middle; i++){
+        left.push_back(list[i]);
+    }
+    for(int i = middle; i < list_size; i++){
+        right.push_back(list[i]);
+    }
+
+    merge_sort(left);
+    merge_sort(right);
+    final_merge(list, left, right);
+
+}
+
+void Sort::final_merge(std::vector<int>& list, std::vector<int> left, std::vector<int> right){
+    int left_size = left.size();
+    int right_size = right.size();
+
+    int left_idx = 0, right_idx = 0, list_idx = 0;
+
+    while( left_idx < left_size && right_idx < right_size){
+        if(left[left_idx] <= right[right_idx]){
+            list[list_idx] = left[left_idx];
+            left_idx++;
+        }
+        else{
+            list[list_idx] = right[right_idx];
+            right_idx++;
+        }
+        list_idx++;
+    }
+    while(left_idx < left_size){
+        list[list_idx] = left[left_idx];
+        left_idx++;
+        list_idx++;
+    }
+    while(right_idx < right_size){
+        list[list_idx] = right[right_idx];
+        right_idx++;
+        list_idx++;
+    }
+
+}
+
 void Sort::print(){
     for (int i = 0; i < num_of_elements; i++)
     {
